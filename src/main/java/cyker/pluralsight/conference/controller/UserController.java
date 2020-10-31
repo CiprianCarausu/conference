0501b2d33f23c75131312aac3a61f38ab379e2b6
Ciprian Carausu <carausuciprian@gmail.com>
2020-10-31 10:50:28 +0200

    user model/controller added

---
+++ src/main/java/cyker/pluralsight/conference/controller/UserController.java
@@ -0,0 +1,29 @@
+package cyker.pluralsight.conference.controller;
+
+
+import cyker.pluralsight.conference.model.User;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserController {
+
+    @GetMapping("/user")
+    public User getUser(@RequestParam(value = "firstname", defaultValue = "Bryan") String firstname,
+                        @RequestParam(value = "lastname", defaultValue = "Hansen") String lastname,
+                        @RequestParam(value = "age", defaultValue = "43") int age) {
+        User user = new User();
+        user.setFirstName(firstname);
+        user.setLastName(lastname);
+        user.setAge(age);
+        return user;
+    }
+
+    @PostMapping("/user")
+    public User postUser(User user){
+        System.out.println("User firstname:" + user.getFirstName());
+        return user;
+    }
+}
--- src/main/webapp/WEB-INF/jsp/registration.jsp
+++ src/main/webapp/WEB-INF/jsp/registration.jsp
@@ -28,9 +28,12 @@
             <td>
                 <form:input path="name"/>
             </td>
+            <td>
+                <form:errors path="name" cssClass="error"/>
+            </td>
         </tr>
         <tr>
-            <td colspan="2">
+            <td colspan="3">
                 <input type="submit" value="Add Registration">
             </td>
 
